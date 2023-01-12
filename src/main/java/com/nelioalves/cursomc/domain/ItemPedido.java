c89f882b8378c2a91d8c3ce7a4a0338014f95e3f
Erick <erickdss.es99@gmail.com>
2023-01-12 17:08:51 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
+++ src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
@@ -2,16 +2,19 @@ package com.nelioalves.cursomc.domain;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 
 @Entity
 public class ItemPedido implements Serializable {
 	private static final long serialVersionUID = 1L;
-
+	
+	@JsonIgnore
 	@EmbeddedId
 	private ItemPedidoPK id = new ItemPedidoPK();
-
+	
 	private Double desconto;
 	private Integer quantidade;
 	private Double preco;
@@ -28,6 +31,7 @@ public class ItemPedido implements Serializable {
 		this.preco = preco;
 	}
 
+	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
@@ -35,7 +39,7 @@ public class ItemPedido implements Serializable {
 	public Produto getProduto() {
 		return id.getProduto();
 	}
-
+	
 	public ItemPedidoPK getId() {
 		return id;
 	}
